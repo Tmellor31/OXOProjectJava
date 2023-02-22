04329c81bbcc89a139b80b9630911b9521d3c451
Tom Mellor <tommellor31@gmail.com>
2023-02-22 00:27:44 +0000

    Added some great tests

--- src/test/java/edu/uob/ExampleControllerTests.java
+++ src/test/java/edu/uob/ExampleControllerTests.java
@@ -3,98 +3,130 @@ package edu.uob;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
 import edu.uob.OXOMoveException.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.time.Duration;
 
 class ExampleControllerTests {
-  private OXOModel model;
-  private OXOController controller;
+    private OXOModel model;
+    private OXOController controller;
 
-  // Make a new "standard" (3x3) board before running each test case (i.e. this method runs before every `@Test` method)
-  // In order to test boards of different sizes, winning thresholds or number of players, create a separate test file (without this method in it !)
-  @BeforeEach
-  void setup() {
-    model = new OXOModel(3, 3, 3);
-    model.addPlayer(new OXOPlayer('X'));
-    model.addPlayer(new OXOPlayer('O'));
-    controller = new OXOController(model);
-  }
-
-  // This next method is a utility function that can be used by any of the test methods to _safely_ send a command to the controller
-  void sendCommandToController(String command) {
-      // Try to send a command to the server - call will timeout if it takes too long (in case the server enters an infinite loop)
-      // Note: this is ugly code and includes syntax that you haven't encountered yet
-      String timeoutComment = "Controller took too long to respond (probably stuck in an infinite loop)";
-      assertTimeoutPreemptively(Duration.ofMillis(1000), ()-> controller.handleIncomingCommand(command), timeoutComment);
-  }
-
-  // Test simple move taking and cell claiming functionality
-  @Test
-  void testBasicMoveTaking() throws OXOMoveException {
-    // Find out which player is going to make the first move
-    OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
-    // Make a move
-    sendCommandToController("a1");
-    // Check that A1 (cell [0,0] on the board) is now "owned" by the first player
-    String failedTestComment = "Cell a1 wasn't claimed by the first player";
-    assertEquals(firstMovingPlayer, controller.gameModel.getCellOwner(0, 0), failedTestComment);
-  }
-
-  // Test out basic win detection
-  @Test
-  void testBasicWin() throws OXOMoveException {
-    // Find out which player is going to make the first move (they should be the eventual winner)
-    OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
-    // Make a bunch of moves for the two players
-    sendCommandToController("a1"); // First player
-    sendCommandToController("b1"); // Second player
-    sendCommandToController("a2"); // First player
-    sendCommandToController("b2"); // Second player
-    sendCommandToController("a3"); // First player
-
-    // a1, a2, a3 should be a win for the first player (since players alternate between moves)
-    // Let's check to see whether the first moving player is indeed the winner
-    String failedTestComment = "Winner was expected to be " + firstMovingPlayer.getPlayingLetter() + " but wasn't";
-    assertEquals(firstMovingPlayer, model.getWinner(), failedTestComment);
-  }
-  @Test
-  void testAdditionalPlayers() throws OXOMoveException {
-    model.addPlayer(new OXOPlayer('J'));
-
-    OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
-    sendCommandToController("c1"); //First Player Move
-    OXOPlayer secondMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
-    sendCommandToController("c2"); //Second Player Move
-    OXOPlayer thirdMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
-    sendCommandToController("c3"); //Third Player Move
-    sendCommandToController("b2"); //First Player Again
-    sendCommandToController("b1"); //Second Player
-    sendCommandToController("b3"); //Third Player
-    sendCommandToController("a1"); //First Player
-    sendCommandToController("a2"); //Second Player
-    sendCommandToController("a3"); //Third Player
-
-    String failedTestNotification = "Expected winner was " + thirdMovingPlayer.getPlayingLetter();
-    assertEquals(thirdMovingPlayer, model.getWinner(), failedTestNotification);
-
-    //Only removes first 2 players
-    controller.reset();
-
-    //add 26 players (all letters)
-    for(char c = 'a';c <= 'z'; c++){
-      model.addPlayer(new OXOPlayer(c));
+    // Make a new "standard" (3x3) board before running each test case (i.e. this method runs before every `@Test` method)
+    // In order to test boards of different sizes, winning thresholds or number of players, create a separate test file (without this method in it !)
+    @BeforeEach
+    void setup() {
+        model = new OXOModel(3, 3, 3);
+        model.addPlayer(new OXOPlayer('X'));
+        model.addPlayer(new OXOPlayer('O'));
+        controller = new OXOController(model);
     }
-  }
 
-  // Example of how to test for the throwing of exceptions
-  @Test
-  void testInvalidIdentifierException() throws OXOMoveException {
-    // Check that the controller throws a suitable exception when it gets an invalid command
-    String failedTestComment = "Controller failed to throw an InvalidIdentifierLengthException for command `abc123`";
-    // The next lins is a bit ugly, but it is the easiest way to test exceptions (soz)
-    assertThrows(InvalidIdentifierLengthException.class, ()-> sendCommandToController("abc123"), failedTestComment);
-    assertThrows(InvalidIdentifierLengthException.class, ()-> controller.handleIncomingCommand("aa1"));
-  }
+    // This next method is a utility function that can be used by any of the test methods to _safely_ send a command to the controller
+    void sendCommandToController(String command) {
+        // Try to send a command to the server - call will timeout if it takes too long (in case the server enters an infinite loop)
+        // Note: this is ugly code and includes syntax that you haven't encountered yet
+        String timeoutComment = "Controller took too long to respond (probably stuck in an infinite loop)";
+        assertTimeoutPreemptively(Duration.ofMillis(1000), () -> controller.handleIncomingCommand(command), timeoutComment);
+    }
+
+    // Test simple move taking and cell claiming functionality
+    @Test
+    void testBasicMoveTaking() throws OXOMoveException {
+        // Find out which player is going to make the first move
+        OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        // Make a move
+        sendCommandToController("a1");
+        // Check that A1 (cell [0,0] on the board) is now "owned" by the first player
+        String failedTestComment = "Cell a1 wasn't claimed by the first player";
+        assertEquals(firstMovingPlayer, controller.gameModel.getCellOwner(0, 0), failedTestComment);
+    }
+
+    // Test out basic win detection
+    @Test
+    void testBasicWin() throws OXOMoveException {
+        // Find out which player is going to make the first move (they should be the eventual winner)
+        OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        // Make a bunch of moves for the two players
+        sendCommandToController("a1"); // First player
+        sendCommandToController("b1"); // Second player
+        sendCommandToController("a2"); // First player
+        sendCommandToController("b2"); // Second player
+        sendCommandToController("a3"); // First player
+
+        // a1, a2, a3 should be a win for the first player (since players alternate between moves)
+        // Let's check to see whether the first moving player is indeed the winner
+        String failedTestComment = "Winner was expected to be " + firstMovingPlayer.getPlayingLetter() + " but wasn't";
+        assertEquals(firstMovingPlayer, model.getWinner(), failedTestComment);
+    }
+
+    @Test
+    void testMultiplePlayerWin() throws OXOMoveException {
+        model = new OXOModel(3, 3, 3);
+        model.addPlayer(new OXOPlayer('X'));
+        model.addPlayer(new OXOPlayer('O'));
+        model.addPlayer(new OXOPlayer('Z'));
+        controller = new OXOController(model);
+        // Find out which player is going to make the first move
+        OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        // Make a bunch of moves for the players
+        sendCommandToController("a1"); // First player
+        sendCommandToController("b1"); // Second player
+        sendCommandToController("a2"); // Third player
+        sendCommandToController("b2"); // First player
+        sendCommandToController("a3"); // Second player
+        sendCommandToController("b3"); // Third player
+
+        String failedTestComment = "Winner was expected to be " + firstMovingPlayer.getPlayingLetter() + " but wasn't";
+        assertEquals(firstMovingPlayer, model.getWinner(), failedTestComment);
+    }
+
+    @Test
+    void testAdditionalPlayers() throws OXOMoveException {
+        model = new OXOModel(5, 5, 4);
+        model.addPlayer(new OXOPlayer('X'));
+        model.addPlayer(new OXOPlayer('O'));
+        model.addPlayer(new OXOPlayer('Z'));
+        model.addPlayer(new OXOPlayer('J'));
+        controller = new OXOController(model);
+
+        OXOPlayer firstMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        sendCommandToController("a1"); //First Player Move
+        OXOPlayer secondMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        sendCommandToController("a2"); //Second Player Move
+        OXOPlayer thirdMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        sendCommandToController("a3"); //Third Player Move
+        OXOPlayer fourthMovingPlayer = model.getPlayerByNumber(model.getCurrentPlayerNumber());
+        sendCommandToController("a4"); //Fourth Player
+        sendCommandToController("b1"); //First Player Move
+        sendCommandToController("b2"); //Second Player Move
+        sendCommandToController("b3"); //Third Player Move
+        sendCommandToController("b4"); //Fourth Player
+        sendCommandToController("c5"); //First Player Move
+        sendCommandToController("b5"); //Second Player Move
+        sendCommandToController("c3"); //Third Player Move
+        sendCommandToController("c4"); //Fourth Player
+        sendCommandToController("d1"); //First Player Move
+        sendCommandToController("d2"); //Second Player Move
+        sendCommandToController("d3"); //Third Player Move
+        sendCommandToController("d4"); //Third Player Move
+
+        // Check that the third player has won
+        String failedTestComment = "Winner was expected to be " + thirdMovingPlayer.getPlayingLetter() + " but wasn't";
+        assertEquals(thirdMovingPlayer, model.getWinner(), failedTestComment);
+    }
+
+
+
+    // Example of how to test for the throwing of exceptions
+    @Test
+    void testInvalidIdentifierException() throws OXOMoveException {
+        // Check that the controller throws a suitable exception when it gets an invalid command
+        String failedTestComment = "Controller failed to throw an InvalidIdentifierLengthException for command `abc123`";
+        // The next lins is a bit ugly, but it is the easiest way to test exceptions (soz)
+        assertThrows(InvalidIdentifierLengthException.class, () -> sendCommandToController("abc123"), failedTestComment);
+        assertThrows(InvalidIdentifierLengthException.class, () -> controller.handleIncomingCommand("aa1"));
+    }
 }
